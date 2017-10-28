37517f817a3cba12063c1507699430847a10ea01
Darren Kwong <kwongdarren@gmail.com>
2017-10-27 22:08:05 -0700

    working code

--- public/assets/js/javascript.js
+++ public/assets/js/javascript.js
@@ -1,16 +1,36 @@
 $(function(){
 
-	$('.submit').click(function(){
+	// $('.submit').click(function(){
+	// 	event.preventDefault();
+	// 	console.log('this works!');
+	// 	var value = $('#text-area').val();
+	// 	console.log(value);
+
+	// 	var newBurger = {
+	// 		burger_name: value
+	// 	};
+	// 	//send post
+	// 	$.ajax("/api/burgers", {
+	// 		type:"POST",
+	// 		data: newBurger
+	// 	}).then(
+	// 		function() {
+	// 			console.log("created new burger");
+	// 			location.reload();
+	// 		});
+	// });
+
+	$(".create-form").on("submit", function(event) {
 		event.preventDefault();
 		console.log('this works!');
 		var value = $('#text-area').val();
 		console.log(value);
 
 		var newBurger = {
-			burger_name: value
-		}
+			burger_name: value,
+		};
 		//send post
-		$.ajax("/api/burger",{
+		$.ajax("/api/burgers", {
 			type:"POST",
 			data: newBurger
 		}).then(
@@ -19,6 +39,28 @@ $(function(){
 				location.reload();
 			});
 	});
+
+//use this for update event
+	$(".devour").on("click", function(event) {
+    var id = $(this).data("id");
+    // var newBurger = $(this).data("newsleep");
+
+    var newBurgerState = {
+      devoured: true
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newBurgerState
+    }).then(
+      function() {
+        console.log("changed devoured", true);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
 
 });
 
